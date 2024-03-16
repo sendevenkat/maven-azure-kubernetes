1e3167f716e043df06eeac94662cc92c075dd029
951595 <gudurukrishn98@gmail.com>
2024-03-16 12:24:23 +0530

    first commit

---
+++ src/main/java/com/example/demo/LtimgenieAzuredevopsApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LtimgenieAzuredevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtimgenieAzuredevopsApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String Hello() {
+		return "welcome to ltimindtree " ;
+	}
+
+}
